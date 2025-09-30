084c22a38f70d8bb1e90607417a481942a3914ff
Semir Zahirovic <semir_guzonja@hotmail.se>
2025-09-30 11:50:05 +0200

    Added in-memory collections entries and entriesByDateTime

--- Diary/Program.cs
+++ Diary/Program.cs
@@ -1,7 +1,11 @@
 ﻿namespace Diary
 {
-    internal class Program
+    class Program
     {
+        private static List<DiaryEntry> entries = new List<DiaryEntry>();
+        private static Dictionary<DateTime, DiaryEntry> entriesByDateTime = new Dictionary<DateTime, DiaryEntry>();
+
+        private const string FilePath = "Diary.json";
         static void Main(string[] args)
         {
             
