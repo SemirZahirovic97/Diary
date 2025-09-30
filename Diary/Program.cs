6a3623a01cb48d6bca09c9fe2c435149f26f6802
Semir Zahirovic <semir_guzonja@hotmail.se>
2025-09-30 11:52:44 +0200

    implement main menu and loop

--- Diary/Program.cs
+++ Diary/Program.cs
@@ -8,7 +8,25 @@
         private const string FilePath = "Diary.json";
         static void Main(string[] args)
         {
-            
-        }
+
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Dagboksapp ===");
+                Console.WriteLine("1) Skriv ny anteckning");
+                Console.WriteLine("2) Lista alla anteckningar");
+                Console.WriteLine("3) Sök anteckning på datum");
+                Console.WriteLine("4) Redigera anteckning");
+                Console.WriteLine("5) Ta bort anteckning");
+                Console.WriteLine("6) Spara till fil");
+                Console.WriteLine("7) Läs från fil");
+                Console.WriteLine("0) Avsluta");
+                Console.Write("Välj: ");
+
+                string choice = Console.ReadLine();
+
+            }
     }
 }
